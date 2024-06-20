cfb815a93943870eb69f6131e2e8326dfe67e19d
zy <zy@git.com>
2024-06-20 14:15:49 +0800

    第二次提交

---
+++ src/main/java/gitfirst.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: gitfirst
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author zy
+ * @Create 2024/6/20 14:14
+ * @Version 1.0
+ */
+public class gitfirst {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
